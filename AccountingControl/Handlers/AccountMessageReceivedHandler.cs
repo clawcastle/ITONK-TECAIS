043e5ad81be475c8717864a0dbec1b75ec12bbda
Tobias Saaby Steffensen <tobias.saaby.steffensen@post.au.dk>
2019-06-01 00:12:07 +0200

    New model

--- AccountingControl/Handlers/AccountMessageReceivedHandler.cs
+++ AccountingControl/Handlers/AccountMessageReceivedHandler.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using TECAIS.RabbitMq;
 using TECAIS.AccountingControl.Models.Events;
+using AccountingControl.Models;
 
 namespace AccountingControl.Handlers
 {
@@ -12,6 +13,9 @@ namespace AccountingControl.Handlers
         public Task Handle(AccountingMessage @event)
         {
             Console.WriteLine($"Received message with status {@event.Amount}");
+            var AccInfo = new AccountingInformation();
+            AccInfo.ConsumerId = @event.ChargingInformation.ConsumerId;
+            AccInfo.ElectricityCost = @event.PricingInformation.Price;
             return Task.CompletedTask;
         }
     }
