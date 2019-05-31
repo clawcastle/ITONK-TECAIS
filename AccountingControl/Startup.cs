d7d6e7e49e1213372ba8a921f06dca45ef840d9f
Tobias Saaby Steffensen <tobias.saaby.steffensen@post.au.dk>
2019-05-31 20:36:51 +0200

    Added a dependency

--- AccountingControl/Startup.cs
+++ AccountingControl/Startup.cs
@@ -10,6 +10,7 @@ using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using TECAIS.AccountingControl.Extensions;
 using TECAIS.RabbitMq;
 
 namespace AccountingControl
